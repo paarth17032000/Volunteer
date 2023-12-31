a57e3b6ec2f4b74442fa4df72c6a9ce8a5d0f0e4
paarth17032000 <paarth17032000@gmail.com>
2023-12-31 14:56:26 +0530

    generic route placed at end

--- server/server.js
+++ server/server.js
@@ -1,38 +1,38 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const logger  = require('./middleware/logger')
-const { corsOptions } = require('./config/utils')
-const mongoose = require('mongoose')
-const connectDB = require('./config/connectDB')
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const logger = require("./middleware/logger");
+const { corsOptions } = require("./config/utils");
+const mongoose = require("mongoose");
+const connectDB = require("./config/connectDB");
 
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 8000;
 
-connectDB()
+connectDB();
 
 // to use formData: 'content-type': 'application/data'
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }));
 // to use json data
-app.use(express.json())
+app.use(express.json());
 
-app.use(logger)
+app.use(logger);
 
 app.use(cors(corsOptions));
 
-app.use('/api/', (req, res) => res.send('Welcome all volunteers API.'))
-app.use('/api/login', require('./routes/api/login'))
-app.use('/api/register', require('./routes/api/register'))
-app.use('/api/events', require('./routes/api/events'))
+app.use("/api/login", require("./routes/api/login"));
+app.use("/api/register", require("./routes/api/register"));
+app.use("/api/events", require("./routes/api/events"));
+app.use("/api/", (req, res) => res.send("Welcome all volunteers API."));
 
 app.use(function (err, req, res, next) {
-    console.log(err)
-    res.status(500).send(err.message)
-})
+	console.log(err);
+	res.status(500).send(err.message);
+});
 
-mongoose.connection.once('open', () => {
-    console.log('Database successfully connected.')
-    app.listen(PORT, () => {
-        console.log(`listening on port ${PORT}`)
-    })
-})
+mongoose.connection.once("open", () => {
+	console.log("Database successfully connected.");
+	app.listen(PORT, () => {
+		console.log(`listening on port ${PORT}`);
+	});
+});
