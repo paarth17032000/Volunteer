47a7646ead7de864fa7d2c16eeec65e8c8c64b60
paarth17032000 <paarth17032000@gmail.com>
2024-01-17 23:28:32 +0530

    removed comments

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -51,14 +51,3 @@ export default function Home() {
 		</main>
 	);
 }
-// let btns = document.querySelectorAll('a')
-// btns.forEach(btn => {
-// 	btn.onmousemove = function(e){
-// 		let x = e.pageX - btn.offsetLeft
-// 		let y = e.pageY - btn.offsetTop
-
-// 		btn.style.setProperty('--x', x+'px')
-// 		btn.style.setProperty('--y', y+'px')
-// 	}
-// })
-// <a href="#"><span>Create New Life</span></a>
