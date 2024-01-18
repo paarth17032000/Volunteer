8f178859adfd9f14088d70e0847a6fa092900a37
paarth17032000 <paarth17032000@gmail.com>
2024-01-18 23:27:00 +0530

    template code for upvote api and mutation added

---
+++ client/api/events/_upvoteEvent.ts
@@ -0,0 +1,5 @@
+import apiRequestInstance from "@/utils/helperFunctions/AxiosInstance";
+
+export const upvoteEvent = async (payload: { eventId: string; userId: string }) => {
+	const result = apiRequestInstance.post(``);
+};
---
+++ client/utils/hooks/mutations/events/useUpvoteEvent.ts
@@ -0,0 +1,20 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { upvoteEvent } from "@/api/events/_upvoteEvent";
+import { toastMessage } from "@/utils/Toast";
+
+const useUpvoteEvent = () => {
+	const queryClient = useQueryClient();
+	return useMutation({
+		mutationFn: upvoteEvent,
+		onSuccess: (data) => {
+			queryClient.invalidateQueries({ queryKey: ["user"] });
+			queryClient.invalidateQueries({ queryKey: ["events"] });
+			toastMessage("Event is upvoted successfully!", "success");
+		},
+		onError: () => {
+			toastMessage("Event upvote failed.", "error");
+		},
+	});
+};
+
+export default useUpvoteEvent;
